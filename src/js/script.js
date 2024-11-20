639bb5135b34a3c9cf31edb8820341032404e3b3
achoule2 <150043943+achoule2@users.noreply.github.com>
2024-11-20 08:20:52 +0100

    ajout code java script ancres

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,10 @@ const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next");
 
-
 const premierItem = document.querySelector(".carousel__item");
 const scrollAmount = premierItem.clientWidth;
 
 if (carousel) {
-  
   prevButton.addEventListener("click", () => {
     carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });
   });
@@ -16,3 +14,15 @@ if (carousel) {
     carousel.scrollBy({ left: scrollAmount, behavior: "smooth" });
   });
 }
+
+
+document.querySelectorAll(".carousel__nav a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault(); 
+    const targetId = link.getAttribute("href").substring(1); 
+    const targetElement = document.getElementById(targetId);
+
+    
+    targetElement.scrollIntoView({ behavior: "smooth", block: "center" });
+  });
+});
